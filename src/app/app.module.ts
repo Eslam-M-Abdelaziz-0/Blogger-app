a901a0d1268dbf2ac197e767158155849f6b68f3
Esalm Mohammed <i.mohammed14.44@outlook.com>
2017-09-15 07:50:24 +0200

    first uploaded files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+//////
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+import { PostsPage } from '../pages/posts/posts';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+//////
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +23,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+	SignupPage,
+    PostsPage,
+    ResetPasswordPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +39,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+	SignupPage,
+    PostsPage,
+    ResetPasswordPage
   ],
   providers: [
     StatusBar,
